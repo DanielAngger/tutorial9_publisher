1fa9b9c72263ff9b9199c1a23cd0c59a0e72ae2a
DanielAngger <angger.7d@gmail.com>
2025-05-08 19:55:52 +0700

    Initial publisher code.

---
+++ target/debug/deps/publisher-2fe3819cb9888938.d
@@ -0,0 +1,5 @@
+/Users/danielangger/Documents/Semester 4/adpro/tutorial9_adpro/publisher/target/debug/deps/publisher-2fe3819cb9888938: src/main.rs
+
+/Users/danielangger/Documents/Semester 4/adpro/tutorial9_adpro/publisher/target/debug/deps/publisher-2fe3819cb9888938.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/publisher.d
@@ -0,0 +1,1 @@
+/Users/danielangger/Documents/Semester\ 4/adpro/tutorial9_adpro/publisher/target/debug/publisher: /Users/danielangger/Documents/Semester\ 4/adpro/tutorial9_adpro/publisher/src/main.rs
